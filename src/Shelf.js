7ef16f01723b654b4b4acc9af9ec3b013703667b
Judith Dea <JudithFoe@gmail.com>
2018-09-10 17:57:19 -0400

    split content up further into components

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class Shelf extends Component {}
+class Shelf extends Component {
+    render() {
+        return()
+    }
+}
 
 export default Shelf;
