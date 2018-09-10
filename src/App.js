c8a3b9ad28e4cd76c20e9f0503556cdeb7337562
Judith Dea <JudithFoe@gmail.com>
2018-09-10 18:10:01 -0400

    added getAll method to fetch books from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import { Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./App.css";
@@ -7,7 +7,16 @@ import SearchPage from "./SearchPage";
 import MainPage from "./MainPage";
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    booklist: []
+  };
+
+  // FETCHING THE BOOKS
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
