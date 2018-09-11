1e301fed94e84bf09ada7cea8d151e4607030324
Judith Dea <JudithFoe@gmail.com>
2018-09-11 19:50:42 -0400

    added state to search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import escapeRegExp from "escape-string-regexp";
+import Book from "./Book";
 
 class SearchPage extends Component {
+  state = {
+    query: ""
+  };
+
+  updateQuery = query => {
+    this.setState({ query });
+  };
+
   render() {
+    const { query, books } = this.state;
+
+    // let showingBooks;
+    // if (query) {
+    //   const match = new RegExp(escapeRegExp(query), "i");
+    //   showingBooks = books.filter(book => match.test(book));
+    // } else {
+    //   showingBooks = books;
+    // }
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">
             Close
           </Link>
+          {/* {JSON.stringify(this.state)} */}
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={e => this.updateQuery(e.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid">
+            {/* <li>{<Book book={book} />}</li> */}
+          </ol>
         </div>
       </div>
     );
